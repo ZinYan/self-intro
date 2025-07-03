2777ac7df9d5020c0506c5494f4409a12797afa7
ZinYan <zinwaiyan2226@gmail.com>
2025-07-03 15:10:16 +0900

    자기소개 페이지 완성

--- script.js
+++ script.js
@@ -12,60 +12,96 @@ profilePhoto.addEventListener("click", ()=>{
 })
 
 
-/* 함수화 */
-const sections = document.querySelectorAll(".right_container section");
-let currentIndex = 0;
+fetch("https://m.search.naver.com/p/csearch/content/apirender.nhn?where=nexearch&pkid=387&u2=20020226&q=생년월일+운세&u1=f&u3=solar&u4=12&_=1719518803829")
+    .then((response) => response.json())  // 응답을 JSON으로 파싱
+    .then(data => {
+        const htmlString = data.flick[0]; // 첫 번째 항목 선택
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlString, 'text/html');
+        const fortune = doc.querySelector('dd b').textContent;
+        const fortuneText = doc.querySelector('dd p').textContent;
+        console.log(fortune); // 추출한 텍스트 출력
+        console.log(fortuneText); // 추출한 텍스트 출력
 
-sections.forEach((section, index) => {
-  section.style.display = index === 0 ? "flex" : "none";
-});
+        const fortuneSection = document.createElement("section");
+        const sectionTitle = document.createElement("h2")
+        sectionTitle.textContent = "오늘의 운세";
+        const fortuneE = document.createElement("h3");
+        fortuneE.style.margin = 0;
+        fortuneE.textContent = fortune;
+        const fortuneTextE = document.createElement("p");
+        fortuneTextE.textContent = fortuneText;
 
-const showAfterSection = () => {
-    sections.forEach((section) => { section.style.display = 'none'; })  
-    if (currentIndex == sections.length - 1) {
-        currentIndex = 0;
-    } else {
-        currentIndex++;
-    }
-    sections[currentIndex].style.display = 'flex';  
-}
-const showBeforeSection = () => {
-    sections.forEach((section) => { section.style.display = 'none'; })
-    if (currentIndex == 0) {
-        currentIndex = sections.length - 1;
-    } else {
-        currentIndex--;
-    }
-    sections[currentIndex].style.display = 'flex';
-}
+        fortuneSection.append(sectionTitle);
+        fortuneSection.append(fortuneE);
+        fortuneSection.append(fortuneTextE);
 
-
-
-let intervalId = setInterval(showAfterSection, 3000);
-
-const resetInterval = () => {
-    clearInterval(intervalId);
-    intervalId = setInterval(showAfterSection, 3000);
-}
-
-sections.forEach((section, index) => {
-    section.addEventListener("click", (event) => {
-        const sectionWidth = section.offsetWidth;
-        const clickX = event.clientX - section.getBoundingClientRect().left;
-
-        if (clickX < sectionWidth / 3) {  
-            showBeforeSection();
-            resetInterval();
-        } else if (clickX > sectionWidth * 2 / 3) {  
-            showAfterSection();
-            resetInterval();
-        } else { 
-            if (intervalId) {
-                clearInterval(intervalId);  
-                intervalId = null;
-            } else {
-                intervalId = setInterval(showAfterSection, 3000);  
-            }
-        }
+        const contactSection = document.querySelector(".contact");
+        contactSection.after(fortuneSection);
+        initializeSections();
     });
-});
+
+
+
+/* 함수화 */
+function initializeSections(){
+    const sections = document.querySelectorAll(".right_container section");
+    let currentIndex = 0;
+
+    sections.forEach((section, index) => {
+    section.style.display = index === 0 ? "flex" : "none";
+    });
+
+    const showAfterSection = () => {
+        sections.forEach((section) => { section.style.display = 'none'; })  
+        if (currentIndex == sections.length - 1) {
+            currentIndex = 0;
+        } else {
+            currentIndex++;
+        }
+        sections[currentIndex].style.display = 'flex';  
+    }
+    const showBeforeSection = () => {
+        sections.forEach((section) => { section.style.display = 'none'; })
+        if (currentIndex == 0) {
+            currentIndex = sections.length - 1;
+        } else {
+            currentIndex--;
+        }
+        sections[currentIndex].style.display = 'flex';
+    }
+
+
+
+    let intervalId = setInterval(showAfterSection, 3000);
+
+    const resetInterval = () => {
+        clearInterval(intervalId);
+        intervalId = setInterval(showAfterSection, 3000);
+    }
+
+    sections.forEach((section, index) => {
+        section.addEventListener("click", (event) => {
+            const sectionWidth = section.offsetWidth;
+            const clickX = event.clientX - section.getBoundingClientRect().left;
+
+            if (clickX < sectionWidth / 3) {  
+                showBeforeSection();
+                resetInterval();
+            } else if (clickX > sectionWidth * 2 / 3) {  
+                showAfterSection();
+                resetInterval();
+            } else { 
+                if (intervalId) {
+                    clearInterval(intervalId);  
+                    intervalId = null;
+                } else {
+                    intervalId = setInterval(showAfterSection, 3000);  
+                }
+            }
+        });
+    });
+
+
+
+}
